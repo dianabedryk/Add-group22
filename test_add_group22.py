6cc8253ee6016136ededb0e7911c5d38eba53add
diana <diana.bedryk@gmail.com>
2022-04-22 12:32:43 -0500

    refactor extract method

--- test_add_group22.py
+++ test_add_group22.py
@@ -9,19 +9,23 @@ class TestAddGroup11(unittest.TestCase):
 
     def test_add_group11(self):
         wd = self.wd
-        #open home page
-        wd.get("http://localhost/addressbook/")
-        #login
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        #open groups page
-        wd.find_element_by_link_text("groups").click()
-        #init group creation
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.group_creation(wd)
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def group_creation(self, wd):
+        # init group creation
         wd.find_element_by_name("new").click()
-        #fill group form
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("gfuy")
@@ -31,13 +35,21 @@ class TestAddGroup11(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("fgh")
-        #submit group creation
+        # submit group creation
         wd.find_element_by_name("submit").click()
-        #return to groups page
-        wd.find_element_by_link_text("group page").click()
-        #logout
-        wd.find_element_by_link_text("Logout").click()
 
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def tearDown(self):
         self.wd.quit()
