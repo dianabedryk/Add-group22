e55e4f3f78736f51670e14394a8d232db66cdce1
diana <diana.bedryk@gmail.com>
2022-04-26 16:11:03 -0500

    Add contact file, class Contact

--- contact.py
+++ contact.py
@@ -1,7 +1,7 @@
 
 class Contact:
     def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home,
-                                    mobil, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear,
+                                    mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear,
                                     address2, phone2, notes):
         self.firstname = firstname
         self.middlename = middlename
@@ -11,7 +11,7 @@ class Contact:
         self.company = company
         self.address = address
         self.home = home
-        self.mobil = mobil
+        self.mobile = mobile
         self.work = work
         self.fax = fax
         self.email = email
