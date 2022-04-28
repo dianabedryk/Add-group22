f13b8f46e0276718212ad8bcf1a656f9ec87c735
diana <diana.bedryk@gmail.com>
2022-04-28 14:14:28 -0500

    Created new test delete first group and create new file: conftest

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,21 +1,15 @@
 # -*- coding: utf-8 -*-
-import pytest
+
 from model.group import Group
-from fixture.application import Application
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
-def test_add_group11(app):
+
+def test_add_group(app):
     app.manager_group.login(username="admin", password="secret")
     app.manager_group.create(Group(name="gfuy", header="ihhn", footer="fgh"))
     app.manager_group.logout()
 
-def test_add_empty_group11(app):
+def test_add_empty_group(app):
     app.manager_group.login(username="admin", password="secret")
     app.manager_group.create(Group(name="", header="", footer=""))
     app.manager_group.logout()
