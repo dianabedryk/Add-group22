14f914cb7b739abb7e671373b5767c61daff420b
diana <diana.bedryk@gmail.com>
2022-04-28 20:53:51 -0500

    homework # 5

--- test/Add_Contact.py
+++ test/Add_Contact.py
@@ -2,12 +2,14 @@
 from model.contact import Contact
 
 
+
 def test_add_contact(app):
     app.manager.login(username="admin", password="secret")
     app.manager.fill_add_address_book_entry(Contact(firstname="dfg", middlename="kmn", lastname="yhj", nickname="rgi",
                                          title="olk", company="tyj", address="iolkn", home_phone="tyhk", mobile_phone="fgbnm",
                                          work_phone="rtjl", fax="vgythk", email="tyjkm", email2="tyhbnmk", email3="yujkl,",
                                          homepage="tyhklm", bday="15", bmonth="March", byear="1980", aday="14",
+
                                          amonth="August", ayear="2010", address2="thjmb", home_phone2="tyjmn",
                                          notes="cvbnyt"))
     app.manager.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,17 +1,12 @@
-# -*- coding: utf-8 -*-
-
 from model.group import Group
 
 
-
 def test_add_group(app):
-    app.manager_group.login(username="admin", password="secret")
-    app.manager_group.create(Group(name="gfuy", header="ihhn", footer="fgh"))
-    app.manager_group.logout()
+    app.manager.login(username="admin", password="secret")
+    app.manager.create(Group(name="gfuy", header="ihhn", footer="fgh"))
+    app.manager.logout()
 
 def test_add_empty_group(app):
-    app.manager_group.login(username="admin", password="secret")
-    app.manager_group.create(Group(name="", header="", footer=""))
-    app.manager_group.logout()
-
-
+    app.manager.login(username="admin", password="secret")
+    app.manager.create(Group(name="", header="", footer=""))
+    app.manager.logout()
