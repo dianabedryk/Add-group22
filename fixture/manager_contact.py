eb7aa407163f57dc46e6a45cd8c1405646bb9054
diana <diana.bedryk@gmail.com>
2022-06-21 12:14:11 -0500

    13

--- fixture/manager_contact.py
+++ fixture/manager_contact.py
@@ -1,6 +1,7 @@
 
 from selenium.webdriver.support.ui import Select
 from model.contact import Contact
+import re
 
 
 class Manager_contactHelper:
@@ -126,17 +127,20 @@ class Manager_contactHelper:
 
     contact_cache = None
 
+
+
     def get_contact_list(self):
         if self.contact_cache is None:
             wd = self.app.wd
             self.app.open_home_page()
             self.contact_cache = []
-            for row in wd.find_elements_by_name("entry"):
-                cells = row.find_elements_by_tag_name("td")
-                firstname = cells[2].text
-                lastname = cells[1].text
-                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                all_phones = cells[5].text
+            row = wd.find_elements_by_xpath("//tr[position() >1]")
+            for element in row:
+                id = element.find_element_by_xpath(".//td/input[@type='checkbox']").get_attribute("value")
+                lastname = element.find_element_by_xpath(".//td[2]").text
+                firstname = element.find_element_by_xpath(".//td[3]").text
+                all_phones = element.find_element_by_xpath(".//td[6]").text
+
                 self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
                                                   all_phones_from_home_page=all_phones))
         return list(self.contact_cache)
@@ -146,20 +150,28 @@ class Manager_contactHelper:
         self.app.open_home_page()
         wd.find_elements_by_xpath("//tbody/tr/td[7]")[index].click()
 
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home_phone = re.search("H: (.*)", text).group(1)
+        work_phone = re.search("W: (.*)", text).group(1)
+        mobile_phone = re.search("M: (.*)", text).group(1)
+        return Contact(home_phone=home_phone, work_phone=work_phone,
+                       mobile_phone=mobile_phone)
+
     def get_contact_info_from_edit_page(self, index):
         wd = self.app.wd
         self.open_contact_view_by_index(index)
-        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        firstname = wd.find_element_by_name("firstname:").get_attribute("value")
         lastname = wd.find_element_by_name("lastname").get_attribute("value")
         address = wd.find_element_by_name("address").get_attribute("value")
         id = wd.find_element_by_name("id").get_attribute("value")
         home_phone = wd.find_element_by_name("home").get_attribute("value")
         work_phone = wd.find_element_by_name("work").get_attribute("value")
         mobile_phone = wd.find_element_by_name("mobile").get_attribute("value")
-        home_phone2 = wd.find_element_by_name("phone2").get_attribute("value")
         return Contact(firstname=firstname, lastname=lastname, address=address, id=id,
-                       home_phone=home_phone, work_phone=work_phone, mobile_phone=mobile_phone,
-                       home_phone2=home_phone2)
+                       home_phone=home_phone, work_phone=work_phone, mobile_phone=mobile_phone)
 
 
 
